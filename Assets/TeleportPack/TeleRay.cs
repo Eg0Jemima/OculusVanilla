b59872498d84a76aff120782ddf2ed58e857f2dd
Eg0Jemima <Eg0Jemima@gmail.com>
2017-04-25 18:00:17 -0400

    - Added teleport ability to avatar

---
+++ Assets/TeleportPack/TeleRay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (OVRInput.GetDown(OVRInput.Button.Any))
+        {
+            if (Physics.Raycast(ray, out hit, 1000))
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+            if (hit.collider.name.Equals("floor_cut"))
+            {
+                Vector3 teleposition = hit.point;
+                teleposition.y = 20;
+                this.transform.parent.parent.position = teleposition;
+            }
+        }
+        
+    }
+}
