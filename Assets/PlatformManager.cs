37ea1f278a93f41f82c60101c8e9f591217a469c
Eg0Jemima <Eg0Jemima@gmail.com>
2017-04-25 10:49:24 -0400

    - Custom avatar now working

---
+++ Assets/PlatformManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Oculus.Avatar;
+using Oculus.Platform;
+using Oculus.Platform.Models;
+using System.Collections;
+
+public class PlatformManager : MonoBehaviour
+{
+
+    public OvrAvatar myAvatar;
+
+    void Awake()
+    {
+        Oculus.Platform.Core.Initialize();
+        Oculus.Platform.Users.GetLoggedInUser().OnComplete(GetLoggedInUserCallback);
+        //Oculus.Platform.Rooms.CreateAndJoinPrivate2(RoomJoinPolicy.Everyone, 2, null);
+        //Oculus.Platform.Matchmaking.Enqueue("EJRoom");
+        //Oculus.Platform.Matchmaking.JoinRoom(Oculus.Platform.Rooms.GetCurrent().RequestID, false);
+        Oculus.Platform.Request.RunCallbacks();  //avoids race condition with OvrAvatar.cs Start().
+        
+    }
+
+    private void GetLoggedInUserCallback(Message<User> message)
+    {
+        if (!message.IsError) {
+            myAvatar.oculusUserID = message.Data.ID;
+        }
+    }
+}
